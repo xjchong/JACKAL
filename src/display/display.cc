8ed1503b977dcfed19d3aececa2d5ee8371483f2
xjchong <xavier.chong.j@gmail.com>
2017-08-07 10:44:39 -0400

    Change font size and add interface elements

--- src/display/display.cc
+++ src/display/display.cc
@@ -8,25 +8,31 @@
 using namespace std;
 
 
-static const int wall_layer = 1;
-static const int floor_layer = 1; 
-static const int door_layer = 3;
-static const int door_layer2 = 4;
-static const int shadow_layer = 2;
+static const int bg_layer = 0;
+static const int bg_layer2 = 1;
+static const int text_layer = 2;
+static const int wall_layer = 3;
+static const int floor_layer = 3; 
+static const int door_layer = 5;
+static const int door_layer2 = 6;
+static const int shadow_layer = 4;
 
 
 Display::Display(TileGrid &tiles, const int rows, const int cols, const int b_row, const int b_col)
     : tiles{tiles}, rows{rows}, cols{cols}, b_row{b_row}, b_col{b_col}
 {
-    string size = "window: size="+to_string(this->cols)+"x"+to_string(this->rows)+";";
+    string size = "window: size="+to_string(cols*2)+"x"+to_string(rows*2)+";";
     const char *size_c = (size).c_str();
     terminal_set(size_c);
     terminal_set("window.title='JACKAL'");
-    terminal_set("font: fonts/text.ttf, size=32x32");
+    terminal_set("font: fonts/header.ttf, size=16x16");
     terminal_set("0x1000: fonts/dungeon.png, size=16x16, resize=32x32, resize-filter=nearest");
+    terminal_set("0x2000: fonts/interface.png, size=16x16");
     doodad.resize(rows*cols);
     update(tiles, "new board");
-    terminal_print(1, 1, "Hello World!");
+    terminal_layer(text_layer);
+    terminal_print(2, 2, "The skeleton archer fires a poisonous arrow at you! -more-");
+    refreshBackground();
 }
 
 
@@ -62,9 +68,9 @@ void Display::update(Subject &subject, string event){
 
 
 void Display::refreshBoard(){
-    for (int i=1; i<=4; ++i){
+    for (int i=floor_layer; i<=door_layer2; ++i){
         terminal_layer(i);
-        terminal_clear_area(b_col, b_row, tiles.cols, tiles.rows);
+        terminal_clear_area(b_col*2, b_row*2, tiles.cols*2, tiles.rows*2);
     }
     for (int row=0; row<tiles.rows; ++row){
         for (int col=0; col<tiles.cols; ++col){
@@ -72,6 +78,16 @@ void Display::refreshBoard(){
         }
     }
     terminal_refresh();
+}
+
+
+void Display::putTile(const int row, const int col, const int code){
+    terminal_put(col*2, row*2, code);
+}
+
+
+void Display::putBoard(const int row, const int col, const int code){
+    putTile(row+b_row, col+b_col, code);
 }
 
 
@@ -90,10 +106,10 @@ void Display::putTerrain(const int row, const int col){
 void Display::putFloor(const int row, const int col){
     if (tiles.getRep(row, col) == '.'){
         terminal_layer(floor_layer);
-        terminal_put(col+b_col, row+b_row, floor_+doodad[row*tiles.cols +col]);
+        putBoard(row, col, floor_+doodad[row*tiles.cols +col]);
         if (row>0 && (tiles.getRep(row-1, col) == '#' || tiles.getRep(row-1, col) == '+')){
             terminal_layer(shadow_layer);
-            terminal_put(col+b_col, row+b_row, shadow_);
+            putBoard(row, col, shadow_);
         }
     }
 }
@@ -103,13 +119,13 @@ void Display::putWall(const int row, const int col){
     if (tiles.getRep(row, col) == '#'){
         terminal_layer(wall_layer);
         if (row == tiles.rows-1 || tiles.getRep(row+1, col) != '#'){
-            terminal_put(col+b_col, row+b_row, wall_alt_+doodad[row*tiles.cols +col]);
+            putBoard(row, col, wall_alt_+doodad[row*tiles.cols +col]);
         } else if ((col == 0 && tiles.getRep(row, col+1) == '.')
             || (col == tiles.cols-1  && tiles.getRep(row, col-1) == '.')
             || (tiles.getRep(row, col-1) == '.' && tiles.getRep(row, col+1) == '.')){
-            terminal_put(col+b_col, row+b_row, wall_+doodad[row*tiles.cols + col]);
+            putBoard(row, col, wall_+doodad[row*tiles.cols +col]);
         } else {
-            terminal_put(col+b_col, row+b_row, wall_);
+            putBoard(row, col, wall_);
         }
     }
 }
@@ -121,19 +137,51 @@ void Display::putDoor(const int row, const int col){
     int door_alt2 = tiles.getRep(row, col) == '+'? door_close_alt2_ : door_open_alt2_;
 
     terminal_layer(floor_layer);
-    terminal_put(col+b_col, row+b_row, floor_);
+    putBoard(row, col, floor_);
     terminal_layer(door_layer);
-    if (tiles.getRep(row, col-1) != '.' || tiles.getRep(row, col+1) != '.'){
-        terminal_put(col+b_col, row+b_row, door); 
+    if (tiles.getRep(row, col-1) != '.' || tiles.getRep(row, col+1) != '.'){ 
+        putBoard(row, col, door);
     } else {
-        terminal_put(col+b_col, row+b_row, door_alt1);
+        putBoard(row, col, door_alt1);
         terminal_layer(door_layer2);
-        terminal_put(col+b_col, (row-1)+b_row, door_alt2);
+        putBoard(row-1, col, door_alt2);
     }
 
     if (tiles.getRep(row-1, col) == '#'){
         terminal_layer(shadow_layer);
-        terminal_put(col+b_col, row+b_row, shadow_);
+        putBoard(row, col, shadow_);
     }
 
+}
+
+
+void Display::refreshBackground(){
+    terminal_layer(bg_layer);
+    for (int row=0; row<rows*2; ++row){
+        for (int col=0; col<cols*2; ++col){   
+            terminal_put(col, row, base_);
+        }
+    }
+    putWindow(0, 0, rows*2, (tiles.cols+2)*2);
+    putWindow(0, (tiles.cols+2)*2, (rows*2)/3, (cols-(tiles.cols+2))*2);
+    putWindow((rows*2)/3, (tiles.cols+2)*2, (rows*4)/3, (cols-(tiles.cols+2))*2);
+    terminal_refresh();
+}
+
+
+void Display::putWindow(const int y, const int x, const int dy, const int dx){
+    terminal_layer(bg_layer2);
+    for (int row=0; row<dy; ++row){
+        for (int col=0; col<dx; ++col){
+            if (row==0) terminal_put(col+x, row+y, window_n_);
+            else if (row==dy-1) terminal_put(col+x, row+y, window_s_);
+            else if (col==0) terminal_put(col+x, row+y, window_w_);
+            else if (col==dx-1) terminal_put(col+x, row+y, window_e_);
+            else terminal_put(col+x, row+y, window_);
+        }
+    }
+    terminal_put(x, y, window_nw_);
+    terminal_put(x+dx-1, y, window_ne_);
+    terminal_put(x, y+dy-1, window_sw_);
+    terminal_put(x+dx-1, y+dy-1, window_se_);
 }
--- src/display/display.h
+++ src/display/display.h
@@ -5,6 +5,20 @@
 #include <queue>
 #include <vector>
 
+
+// The unicodes for the background
+const int base_ = 0x1060;
+const int window_ = 0x201D;
+const int window_n_ = window_ - 16;
+const int window_s_ = window_ + 16;
+const int window_w_ = window_ - 1;
+const int window_e_ = window_ + 1;
+const int window_nw_ = window_ - 17;
+const int window_ne_ = window_ - 15;
+const int window_sw_ = window_ + 15;
+const int window_se_ = window_ + 17;
+
+// The unicodes for the terrain tiles
 const int floor_ = 0x1077;
 const int wall_ = 0x1026;
 const int wall_alt_ = 0x1020;
@@ -29,20 +43,26 @@ class Display : public Observer{
     std::queue<std::string> messages;
     std::vector<int> doodad;
 
+    void putTile(const int row, const int col, const int code);
     /* These are board displaying functions
      */
+    void putBoard(const int row, const int col, const int code);
     void putFloor(const int row, const int col);
     void putWall(const int row, const int col);
     void putDoor(const int row, const int col);
-    void refreshBoard();
-
     /* putTerrain(row, col) displays a single terrain tile.
      */
     void putTerrain(const int row, const int col);
+    void refreshBoard();
+
+    /* These are background displaying functions
+     */
+    void refreshBackground();
+    void putWindow(const int y, const int x, const int dy, const int dx);
 
 public:
 
-    Display(TileGrid &tiles, const int rows=21, const int cols=80, const int b_row=3, const int b_col=1);
+    Display(TileGrid &tiles, const int rows=30, const int cols=48, const int b_row=4, const int b_col=1);
     ~Display();
 
     void refresh();
